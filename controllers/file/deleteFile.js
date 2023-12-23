20251df49f26a2356b1763edf7ec7e1ebe8c78b3
Fifoll <kakolfilip@wp.pl>
2023-12-23 13:47:15 +0100

    add deleting file by id

--- controllers/file/deleteFile.js
+++ controllers/file/deleteFile.js
@@ -1,5 +1,33 @@
-const deleteFile = () => {
-
+import File from '../../models/file.js';
+const deleteFile = async (req, res) => {
+    try {
+        const file = await File.destroy({
+            where: {
+                user_id: req.userData.userId,
+                file_id: req.params.id
+            }
+        })
+        if(file) {
+            res.status(200).send({
+                success: true,
+                status: res.status,
+                message: `Delete file with id ${req.params.id}`
+            });
+        } else {
+            res.status(400).send({
+                success: false,
+                status: res.status,
+                message: `File with id ${req.params.id} doesnt exist`
+            });
+        }
+    }
+    catch(err) {
+        res.status(500).send({
+            success: false,
+            status: res.status,
+            message: err.message
+        });
+    }
 }
 
 export default deleteFile;
