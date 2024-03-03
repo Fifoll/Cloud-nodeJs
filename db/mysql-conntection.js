c36090ec3b3804b4470cdfd84b97d80d4ed07a90
Fifoll <kakolfilip@wp.pl>
2024-03-03 18:58:29 +0100

    connect with db on server, add enviroment variables

--- db/mysql-conntection.js
+++ db/mysql-conntection.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dbConfig from "../config/db-config.js";
 
 const sequelize = new Sequelize(dbConfig.DB_NAME, dbConfig.DB_USER, dbConfig.DB_PASSWORD, {
-    host: 'localhost',
+    host: dbConfig.DB_HOST,
     dialect: 'mysql'
 });
 
